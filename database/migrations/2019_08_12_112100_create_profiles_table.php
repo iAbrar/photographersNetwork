86c53292c48dbe5f83c07ae18cb79604d2850966
Abrar <abrar.ksu@gmail.com>
2019-08-12 15:18:10 +0300

    feat: Add profile model
    add profile model to the project and hooked it with user

---
+++ database/migrations/2019_08_12_112100_create_profiles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('bio')->nullable();
+            $table->string('url')->nullable();
+            $table->boolean('avaliableToHire')->nullable(); // ask about best way to save this kind of data
+            $table->string('avatar')->nullable();
+            $table->timestamps();
+
+            $table->index('user_id'); // add index to all foreign key
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
