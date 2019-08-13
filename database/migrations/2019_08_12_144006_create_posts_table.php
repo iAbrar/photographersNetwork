5ce30dc911b8ae0308aeffd0dac612d96d0a83d7
Abrar <abrar.ksu@gmail.com>
2019-08-13 13:42:14 +0300

    feat: Add post model
    add post model to the project and hooked it with user

---
+++ database/migrations/2019_08_12_144006_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->text('caption')->nullable();
+            $table->string('image')->nullable();
+            $table->integer('claps')->nullable();
+            $table->timestamps();
+
+            $table->index('user_id'); // add index to all foreign key
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
