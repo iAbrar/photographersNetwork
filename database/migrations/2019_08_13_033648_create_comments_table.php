1560f196183d29e9dea053606c95eaaab4828b30
Abrar <abrar.ksu@gmail.com>
2019-08-13 16:33:15 +0300

    feat: Add comment model
    add comment model to the project and hooked it with post

---
+++ database/migrations/2019_08_13_033648_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('post_id');
+            $table->text('body');
+            $table->timestamps();
+
+            $table->index('post_id'); // add index to all foreign key
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
