7daff62f5bb2103cd28022971da964b5d947170a
Abrar <abrar.ksu@gmail.com>
2019-08-19 19:02:08 +0300

    fix: Order comments
    ordering comments by last add

--- app/Post.php
+++ app/Post.php
@@ -16,7 +16,7 @@ class Post extends Model
 
   public function comments()
   {
-    return $this->hasMany(Comment::class);
+    return $this->hasMany(Comment::class)->latest();
   }
 
   public function addComment($body)
