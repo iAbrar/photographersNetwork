0d7d7b39f039440def5500f4c83449eab95b8a1a
Abrar <abrar.ksu@gmail.com>
2019-08-19 21:34:53 +0300

    style: Change page layout
    change posts.show layout and comment style

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,80 +2,103 @@
 
 @section('content')
 <div class="container">
-    <h3>Show a Post </h3>
-    <div class="row">
-        <div class="col-8">
+    <div class="row justify-content-center">
+        <div class="col-md-10 ">
             <div class="row">
-                <img src="/storage/{{ $post->image }}" class="w-100" alt="">
+                <img src="/storage/{{ $post->image }}" class="img-fluid" alt="">
             </div>
-            <div class="row d-flex justify-content-between">
+            <div class="row d-flex px-3 justify-content-between">
 
                 <div> # claps</div>
                 <div>
                     calender icon {{ Carbon\Carbon::parse($post->created_at)->diffForHumans() }}
                 </div>
             </div>
-            <div class="row">
+            <div class="row px-3">
                 Category: <span class="pl-2">i have to add category of the image</span>
             </div>
-
-
             <hr>
+            <div class="card">
+                <div class="card-body">
+                    <div class="row d-flex justify-content-between align-items-baseline p-3">
+                        <div class="d-flex justify-content-between align-items-baseline">
+                            <div class="pr-2">
+                                <img src="/storage/{{ $post->user->profile->avatar}}" class="rounded-circle" width="50px;" alt="">
+                            </div>
+                            <h5>
+                                By <strong class="pl-1"><a href="/{{ $post->user->username }}">{{ $post->user->username }}</a></strong>
+                            </h5>
+                        </div>
+                        @can ('update', $post)
+                        <div class="dropdown">
+                            <button class="btn btn-primary-outline" type="button" id="more" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="fas fa-ellipsis-h"></i>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="more">
+                                <a class="dropdown-item" href="/posts/{{ $post->id }}/edit">Edit</a>
+                                <form action="/posts/{{$post->id}}" method="post">
+                                    @csrf
+                                    @method('DELETE')
 
+                                    <button class="dropdown-item" type="submit">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                        @endcan
+                    </div>
+                    <div class="px-2 text-justify align-items-baseline">
+                        <p class="card-text">
+                            {!!nl2br(e($post->caption)) !!}
+                            <!-- is this safe way to use lines?-->
+                        </p>
+                        <p>
+                          <small class="text-muted">Last updated {{ Carbon\Carbon::parse($post->updated_at)->diffForHumans() }}</small>
+                        </p>
 
-            <div class="row d-flex align-items-baseline">
-                <div class="pr-2">
-                    <img src="/storage/{{ $post->user->profile->avatar}}" class="rounded-circle" width="50px;" alt="">
+                    </div>
                 </div>
-                by <strong class="pl-2"><a href="/{{ $post->user->username }}">{{ $post->user->username }}</a></strong>
-
             </div>
-            <div class="row pt-2 align-items-baseline">
-                <div>
-                    {!!nl2br(e($post->caption)) !!} <!-- is this safe way to use lines?-->
-                </div>
-                @can ('update', $post)
-                  <div class="dropdown">
-                      <button class="btn btn-primary-outline" type="button" id="more" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <i class="fas fa-ellipsis-h"></i>
-                      </button>
-                      <div class="dropdown-menu" aria-labelledby="more">
-                          <a class="dropdown-item" href="/posts/{{ $post->id }}/edit">Edit</a>
-                          <form action="/posts/{{$post->id}}" method="post">
-                            @csrf
-                            @method('DELETE')
-
-                              <button class="dropdown-item" type="submit">Delete</button>
-                          </form>
-                      </div>
-                  </div>
-                @endcan
-            </div>
-
-            <div class="row pt-2">
-                <h3>Comments</h3>
-
-
-                @foreach ($post->comments as $comment)
-                <div class="col-12">
-                    {{ $comment->body }}
-                </div>
-                <hr />
-                @endforeach
-            </div>
+        </div>
+    </div>
+    <!--comment section -->
+    <div class="row justify-content-center pt-5">
+        <div class="col-md-10">
+            <h3>Write a comments</h3>
             <form action="/posts/{{$post->id}}/comment" method="post">
                 @csrf
 
                 <fieldset class="form-group">
 
-                    <textarea type="text" class="form-control" name="body" placeholder="write a comment"></textarea>
+                    <textarea type="text" class="form-control" name="body" placeholder="your comment"></textarea>
                 </fieldset>
                 <fieldset class="form-group">
-
-                    <button class="btn btn-primary">Add Comment</button>
+                    <button class="btn custom-btn custom-btn-bg custom-btn-link">Add Comment</button>
                 </fieldset>
 
             </form>
+        </div>
+    </div>
+    <hr width="85%">
+
+    <div class="row justify-content-center">
+
+        <div class="col-md-10">
+            @foreach ($post->comments as $comment)
+            <div class="card mb-3">
+                <div class="row no-gutters">
+                    <div class="col-md-2">
+                        <img src="/storage/{{ $post->user->profile->avatar}}" class="card-img rounded-circle p-3" alt="...">
+                    </div>
+                    <div class="col-md-10">
+                        <div class="card-body">
+                            <h5 class="card-title">Username</h5>
+                            <p class="card-text text-justify pr-4">{{ $comment->body }}</p>
+                            <p class="card-text"><small class="text-muted">Last updated {{ Carbon\Carbon::parse($comment->updated_at)->diffForHumans() }}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
