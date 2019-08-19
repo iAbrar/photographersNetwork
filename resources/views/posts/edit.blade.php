5f2c428aaa3f1baaf95883cf9ddc1d7dc9881a62
Abrar <abrar.ksu@gmail.com>
2019-08-19 22:03:45 +0300

    style: Change page layout
    change posts.edit layout and comment style

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -2,32 +2,42 @@
 
 @section('content')
 <div class="container">
-    <h3>Edit Post</h3>
-    <form action="/posts/{{$post->id}}" enctype="multipart/form-data" method="post">
-      @method('PATCH')
-        @csrf <!-- need to remove -->
+    <div class="row justify-content-center">
+        <div class="col-8">
+            <div class="card">
+                <div class="card-header">Edit Post</div>
+                <div class="card-body">
+                    <form action="/posts/{{$post->id}}" enctype="multipart/form-data" method="post">
+                        @method('PATCH')
+                        @csrf <!-- need to remove -->
 
-        <div class="row">
-            <div class="col-8 offset-2">
-                <div class="form-group row">
-                    <label for="caption" class="col-form-label ">Post Caption</label>
+                        <div class="row">
+                            <div class="col-8 offset-2">
+                                <div class="form-group row">
+                                    <label for="caption" class="col-form-label ">Post Caption</label>
 
-                    <textarea id="caption" class="form-control" name="caption">{{ $post->caption }}</textarea>
+                                    <textarea rows="4" id="caption" class="form-control" name="caption">{{ $post->caption }}</textarea>
 
-                    @error('caption')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                    @enderror
+                                    @error('caption')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
 
-                </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-5 custom-btn-group mt-4 ">
+                                <button class="btn custom-btn custom-btn-bg custom-btn-link">Edit Post</button>
+                            </div>
+                        </div>
 
-                <div class="row pt-3">
-                    <button class="btn btn-primary">Edit Post</button>
+                    </form>
                 </div>
             </div>
         </div>
-    </form>
-
+    </div>
 </div>
+
 @endsection
